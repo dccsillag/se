c64a68f099e59995796d52d5b736668cd34006e0
Daniel Csillag <dccsillag@gmail.com>
2021-05-18 15:06:53 -0300

    Rewrite internal file structure

--- se.sh
+++ se.sh
@@ -20,26 +20,31 @@ is_number() {
 ROOTDIR="$HOME/.local/share/se"
 mkdir -p "$ROOTDIR"
 
-list_ids() {
-    for id in $([ -n "$(ls "$ROOTDIR"/*.cmd 2>/dev/null)" ] && find "$ROOTDIR"/*.cmd | sed 's|^.\+/\([-a-z0-9]\+\)\.cmd$|\1|')
-    do
-        [ "$(cat "$ROOTDIR/$id.host")" = "$(hostname)" ] && echo "$id"
-    done
+list_sessions() {
+    find "$ROOTDIR" -maxdepth 1 -type d -name "$(hostname)-*"
 }
 
-get_id() {
-    if is_number "$1"
-    then
-        "$0" -l | sed -n '1~2p' | tac | sed -n "$1p" | sed 's/^.\+] \([-a-z0-9]\+\)  --.\+$/\1/'
-    else
-        echo "$1"
-    fi
+session_exists() {
+    [ -d "$ROOTDIR/$(hostname)-$1" ]
 }
 
-for id in $(list_ids)
+get_session() {
+    session_exists "$1" || {
+        echo "No such session: $1" 1>&2
+        exit 1
+    }
+
+    echo "$ROOTDIR/$(hostname)-$1"
+}
+
+sess_id() {
+    basename "$1" | sed "s/^.\+-\([0-9]\+\)$/\1/"
+}
+
+for sess in $(list_sessions)
 do
-    [ -f "$ROOTDIR/$id.pid" ] || continue
-    ( kill -0 "$(cat "$ROOTDIR/$id.pid")" 2> /dev/null ) || rm "$ROOTDIR/$id.pid" "$ROOTDIR/$id.sh"
+    [ -f "$sess/pid" ] || continue
+    ( kill -0 "$(cat "$sess/pid")" 2> /dev/null ) || rm "$sess/pid" "$sess/run.sh"
 done
 
 is_number "$1" && {
@@ -49,78 +54,88 @@ is_number "$1" && {
 }
 
 case "$1" in
-    -l) for id in $(list_ids)
+    -l) for sess in $(list_sessions)
         do
-            if [ -f "$ROOTDIR/$id.pid" ]
+            if [ -f "$sess/pid" ]
             then
-                printf "[running] %s  --  %s\t%s\n" "$id" "$(head -1 "$ROOTDIR/$id.out")" "$(cat "$ROOTDIR/$id.cmd")"
+                echo "$(sess_id "$sess")\t[running] $(cat "$sess/command.txt")"
+                echo "\t$(cat "$sess/starttime.txt")"
             else
-                printf "[done] %s  --  %s .. %s\t%s\n" "$id" "$(head -1 "$ROOTDIR/$id.out")" "$(tail -1 "$ROOTDIR/$id.out")" "$(cat "$ROOTDIR/$id.cmd")"
+                echo "$(sess_id "$sess")\t[done] $(cat "$sess/command.txt")"
+                echo "\t$(cat "$sess/starttime.txt") -- $(cat "$sess/endtime.txt")"
             fi
-        done | sort | tac | cat -n | tac | sed 's/^\(.\+\t.\+\)\t\(.\+\)/\1\n\t\2/'
+        done
         ;;
     -v) if [ -z "$3" ]
         then
             clear -x
-            se -v "$2" tail -f
+            "$0" -v "$2" tail -f
         else
-            f="$ROOTDIR/$(get_id "$2").out"
+            f="$(get_session "$2")/stdout.txt"
             shift 2
             "$@" "$f"
         fi
         ;;
-    -d)
-        id="$(get_id "$2")"
-        [ -f "$ROOTDIR/$id.pid" ] && {
-            echo "Session is running, and thus cannot be deleted: '$id'."
+    -d) sess="$(get_session "$2")"
+        [ -f "$sess/pid" ] && {
+            echo "Session is running, and thus cannot be deleted: '$2'."
             exit 1
         }
         echo "Are you sure you want to delete this session?"
-        echo "  UUID: $id"
-        echo "  COMMAND: $(cat "$ROOTDIR/$id.cmd")"
-        echo "  STARTED RUNNING:  $(head -1 "$ROOTDIR/$id.out")"
-        echo "  FINISHED RUNNING: $(tail -1 "$ROOTDIR/$id.out")"
+        echo "  COMMAND: $(cat "$sess/command.txt")"
+        echo "  STARTED RUNNING:  $(cat "$sess/starttime.txt")"
+        echo "  FINISHED RUNNING: $(cat "$sess/endtime.txt")"
         echo
         printf '[yn] '
         read -r yn
         case "$yn" in
-            [Yy]*)  echo "Deleting session '$id'" ;;
+            [Yy]*)  echo "Deleting session '$2'" ;;
             [Nn]*)  echo "Aborting."; exit 0 ;;
             *)      echo "Answer was not 'yes' nor 'no'; aborting"; exit 1 ;;
         esac
 
         set -x
-        rm "$ROOTDIR/$id.cmd"
-        rm "$ROOTDIR/$id.out"
-        rm "$ROOTDIR/$id.host"
+        rm -rf "$sess"
         ;;
-    -c) kill -2  "$(cat "$ROOTDIR/$(get_id "$2").pid")" ;;
-    -t) kill -15 "$(cat "$ROOTDIR/$(get_id "$2").pid")" ;;
-    -K) kill -9  "$(cat "$ROOTDIR/$(get_id "$2").pid")" ;;
+    -c) kill -2  "$(cat "$ROOTDIR/$(get_id "$2")/pid")" ;;
+    -t) kill -15 "$(cat "$ROOTDIR/$(get_id "$2")/pid")" ;;
+    -K) kill -9  "$(cat "$ROOTDIR/$(get_id "$2")/pid")" ;;
     -*) echo "Bad flag: $1. See \`se -h\`." ;;
-    *)  id=$(uuidgen)
+    *)  # Get new ID
+        id=0
+        while session_exists "$id"
+        do
+            id=$((id+1))
+        done
 
-        echo date > "$ROOTDIR/$id.sh"
-        echo echo >> "$ROOTDIR/$id.sh"
+        # Setup session files
+        sessdir="$ROOTDIR/$(hostname)-$id"
+        mkdir -p "$sessdir"
+        scriptfile="$sessdir/run.sh"
+        outfile="$sessdir/stdout.txt"
+        pidfile="$sessdir/pid"
+        cmdfile="$sessdir/command.txt"
+
+        # Create shell script to run with nohup
+        echo "date > '$sessdir/starttime.txt'" >> "$scriptfile"
         for arg in "$@"
         do
             printf "'%s' " "$(echo "$arg" | sed s/\'/\'\\\\\'\'/g)"
-        done >> "$ROOTDIR/$id.sh"
-        echo >> "$ROOTDIR/$id.sh"
-        echo echo >> "$ROOTDIR/$id.sh"
-        echo date >> "$ROOTDIR/$id.sh"
-        chmod +x "$ROOTDIR/$id.sh"
+        done >> "$scriptfile"
+        echo >> "$scriptfile"
+        echo "date > '$sessdir/endtime.txt'" >> "$scriptfile"
+        chmod +x "$scriptfile"
 
-        nohup "$ROOTDIR/$id.sh" > "$ROOTDIR/$id.out" 2>&1 &
+        # Run with nohup
+        nohup "$scriptfile" > "$outfile" 2>&1 &
         pid="$!"
-        echo "$pid" > "$ROOTDIR/$id.pid"
-        echo "$(hostname)" > "$ROOTDIR/$id.host"
-        echo "$*" > "$ROOTDIR/$id.cmd"
+        echo "$pid" > "$pidfile"
+        echo "$*" > "$cmdfile"
 
         echo "Running in the background."
         echo "  COMMAND: $*"
         echo "  HOST: $(hostname)"
         echo "  PID: $pid"
-        echo "  UUID: $id"
+        echo "  ID: $id"
         ;;
 esac
